1c7ac44b69c6a0259d1dd78751c2f465a722ff18
ester kim <kimester1996@gmail.com>
2022-05-19 15:10:46 -0800

    finalizing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //grab button
 var startBtn = document.getElementById("start-btn");
 var displayTimeEl = document.getElementById("displayTime");
-var timer = 10;
+var timer = 60;
 var timerInterval;
 var QAScreenEl = document.getElementById("QAScreen");
 var score = document.getElementById("score");
@@ -18,16 +18,20 @@ var quizQuestions = [
     options: ["True", "False"],
     answer: "False",
   },
-
+  {
+    question: "2+2",
+    options: [2, 5, 4, 3],
+    answer: 4,
+  },
 ];
 var index = 0;
 
 //Declare some function
 function startQuiz() {
   alert("Welcome to the Javascript Speeed Test");
-  startBtn.style.display = "none";
-  document.getElementById("startQuiz").style.display = "none";
-
+  // startBtn.style.display = "none";
+  document.getElementById("startQuiz").setAttribute("class", "hide");
+  displayTimeEl.textContent = timer;
   //Start clock
   timerInterval = setInterval(function () {
     timer--;
@@ -37,133 +41,93 @@ function startQuiz() {
 
     //stop the clock
     if (timer === 0) {
-      clearInterval(timerInterval);
-      QAScreenEl.style.display = "none";
-      alert("Time's up!! ");
+      
       endGame();
     }
   }, 1000);
   //Unhide the QA section
-  QAScreenEl.style.display = "block";
-  //display Q and Options
-  if (index <= 1) {
-    showQA();
-  } else {
-    endGame();
-  }
+  QAScreenEl.removeAttribute("class");
+  showQA()
 }
 
 function showQA() {
-  var currentQ = quizQuestions[index].question;
-  document.getElementById("q1").textContent = currentQ;
-  console.log(quizQuestions[index], index);
-  //show the options
-  console.log(index);
-  document.getElementById("01").textContent = quizQuestions[index].options[0];
-  document.getElementById("02").textContent = quizQuestions[index].options[1];
-  //add event listener
-  document.getElementById("01").addEventListener("click", RWAns);
-  document.getElementById("02").addEventListener("click", RWAns);
-  //set it to blank after 2 seconds
-  setInterval(function () {
-    document.getElementById("RW").textContent = "";
-  }, 2000);
+  var currentQ = quizQuestions[index];
+  document.getElementById("q1").textContent = currentQ.question;
 
-  // var currentQ = quizQuestions[index].question;
-  // document.getElementById("q3").textContent = currentQ;
-  // console.log(quizQuestions[index], index);
-  // //show the options
-  // document.getElementById("abc").textContent = quizQuestions[index].options[0];
-  // document.getElementById("bca").textContent = quizQuestions[index].options[1];
-  // document.getElementById("cab").textContent = quizQuestions[index].options[2];
-  // //add event listener
-  // document.getElementById("abc").addEventListener("click", a);
-  // document.getElementById("bca").addEventListener("click", a);
-  // document.getElementById("cab").addEventListener("click", a);
-  // //set it to blank after 2 seconds
-  // setInterval(function () {
-  //   document.getElementById("a").textContent = "";
-  // }, 2000);
+  //show the options
+
+
+  document.getElementById("choices").innerHTML = "";
+
+  currentQ.options.forEach((choice) => {
+    var btn = document.createElement("button");
+    btn.textContent = choice;
+
+    btn.setAttribute("value", choice);
+    btn.onclick = RWAns;
+
+    document.getElementById("choices").append(btn);
+  });
 }
 
 //checking thr answers
 function RWAns() {
-  console.log("button clicked");
+  // console.log("button clicked");
 
   var rightAnswer = quizQuestions[index].answer;
   console.log(rightAnswer);
-  var buttonClicked = this.textContent;
+  var buttonClicked = this.value;
 
-  if (rightAnswer === buttonClicked) {
-    document.getElementById("RW").textContent = "Right Answer";
-    score = score + 2;
-    console.log(score);
-  } else {
-    document.getElementById("RW").textContent = "Wrong Answer";
-    score = score - 2;
-    console.log(score);
+  if (rightAnswer !== buttonClicked) {
+    timer -= 10;
+   
+    if (timer < 0){
+      timer = 0
+    }
+
+
   }
   //increase the index count by 1
-  index = index + 1;
+  index++;
   //show the next question
-  if (index <= 1) {
-    showQA();
+  if (index === quizQuestions.length) {
+      endGame();
   } else {
-    endGame();
+  showQA();
   }
 }
 
-// function endGame() {
-// clearInterval(displaytime)
-// QAScreenEl.style.display = "none"
-// }
-
-// function a() {
-//   console.log("button clicked");
-
-//   var rightAnswer = quizQuestions[index].answer;
-//   console.log(rightAnswer);
-//   var buttonClicked = this.textContent;
-
-//   if (rightAnswer === buttonClicked) {
-//     document.getElementById("a").textContent = "Right Answer";
-//   } else {
-//     document.getElementById("a").textContent = "Wrong Answer";
-//   }
-//   document.getElementById("a").textContent = "Wrong Answer";
-// }
-// //increase the index count by 1
-// index = index + 1;
-// //show the next question
-// showQA();
-
-//EVENT LISTENER ****************
-
-startBtn.addEventListener("click", startQuiz);
 
 
+
+function endGame() {
+  // startBtn.style.display = "block";
+  // startBtn.textContent = "Try Again?";
+  document.getElementById('score').removeAttribute('class')
+  QAScreenEl.setAttribute("class", "hide");
+  clearInterval(timerInterval);
+}
 
 
 // scores.forEach((score) => {
-//   var points = document.createElement("points");
+//   var newli = document.createElement("li");
 //   newli.textContent = `${score.init} : ${score.score}`;
 //   document.querySelector("ol").append(newli);
 // });
 
-function endGame() {
-  startBtn.style.display = "block";
-  startBtn.textContent = "Try Again?";
-  timer = 10;
+function saveScore(e){
+  e.preventDefault()
+  var scores = JSON.parse(localStorage.getItem("score")) || [];
 
+  var userScore = {
+    score: timer,
+    initials: document.getElementById('initialInput').value
+  }
+
+  scores.push(userScore)
+  localStorage.setItem("score", JSON.stringify(scores) )
+  
 }
-
-var scores = JSON.parse(localStorage.getItem("score"))||[];
-
-scores.forEach(score=>{
-    var newli = document.createElement("li");
-    newli.textContent = `${score.init} : ${score.score}`
-    document.querySelector("ol").append(newli)
-})
 //DOM stands for Document Object Model
 //true
 //false
@@ -177,10 +141,8 @@ scores.forEach(score=>{
 // b. only true if both the values given to it are true
 // c. the only values in JS that is not equal to it self
 
-//submit high score with initials
-// console.log(quizQuestions[0]);
-// console.log(quizQuestions[2].question);
-// console.log(quizQuestions[2].options[0]);
-// console.log(quizQuestions[2].options[1]);
-// console.log(quizQuestions[2].options[2]);
-// console.log(quizQuestions[2].options[3]);
+
+//EVENT LISTENER ****************
+
+startBtn.addEventListener("click", startQuiz);
+document.getElementById('initials').addEventListener('submit', saveScore)
